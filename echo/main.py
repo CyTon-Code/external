b0b843bfbf72eb2eb0c6e69a4220606f47107aff
CyTon Code <cytoncode@gmail.com>
2021-03-05 11:38:02 +0200

    upd code
    Signed-off-by: CyTon Code <cytoncode@gmail.com>

--- echo/main.py
+++ echo/main.py
@@ -3,23 +3,25 @@
     Via os.system or return (RUN) - doesn't work.
 """
 
-if __name__ != "__main__":  # If not imported, I exit is the module:
+if __name__ == "__main__":  # If not imported, I exit is the module:
     print("I am is Module!!! Bye Bye!!!")
     exit()  # Answer: I'm leaving, I'm a module.
 
 
 def main(name: str, *value: tuple) -> int:
     """
-        Этот модуль выводит текст
+        Этот plugin выводит текст
         В дальнейшем будем поддерживать кавычки - на самом деле parser не по
         пробелам а кав. перед передачей сюда аргументов.
         Еще нужно подумать о том чтобы использовать вызов команд внутри команд
         например: echo "Hello World" `file_open info.txt` - но нельзя
         использовать плагины одновременно с плагинами.
+        :param name: имя команды плагина, как правило всегда лишние.
+        :param value: кортеж строк для вывода.
+        :return: - возвращает всегда результат числом.
     """
 
     del name
-    for i in value:
-        print(i, end=" ")
+    print(*value)
 
     return 0
